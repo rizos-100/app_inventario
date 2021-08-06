35f426d72d4b19e2fe370e7787fc57735ae5dd95
rizos-100 <50757090+rizos-100@users.noreply.github.com>
2021-08-05 23:40:27 -0500

    Correcion de carpetas

---
+++ OR_LE_INVENTARIO/app/src/main/java/org/rizosdb/utl/or_le_inventario/MainActivity.java
@@ -0,0 +1,14 @@
+package org.rizosdb.utl.or_le_inventario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
